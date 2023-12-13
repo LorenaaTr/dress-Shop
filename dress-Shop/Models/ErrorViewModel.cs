3d8ef7fd194e11a1c53ebf4ed923a9b7792557d7
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-13 13:36:19 +0100

    Add project files.

---
+++ dress-Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dress_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
