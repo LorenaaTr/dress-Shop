c4c77eb62e1baf4624582afcfffce1b2971c9caf
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-19 14:42:33 +0100

    TemoData

---
+++ dress-Shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using dress_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dress_Shop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Elegant", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Simple", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Leather", DisplayOrder = 3 }
+                ); 
+        }
+    }
+}
--- dress-Shop/Models/Category.cs
+++ dress-Shop/Models/Category.cs
@@ -1,6 +1,20 @@
-﻿namespace dress_Shop.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace dress_Shop.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+
+
     }
 }
