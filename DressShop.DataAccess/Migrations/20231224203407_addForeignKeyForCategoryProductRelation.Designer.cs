dddb8949de4cd2e387827c5af369ee5c1a735cc4
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-24 21:36:28 +0100

    .

--- DressShop.DataAccess/Migrations/20231224203407_addForeignKeyForCategoryProductRelation.Designer.cs
+++ DressShop.DataAccess/Migrations/20231224203407_addForeignKeyForCategoryProductRelation.Designer.cs
@@ -3,6 +3,7 @@ using DressShop.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DressShop.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231224203407_addForeignKeyForCategoryProductRelation")]
+    partial class addForeignKeyForCategoryProductRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DressShop.Models/Product.cs
+++ DressShop.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DressShop.Models
 {
@@ -35,6 +36,9 @@ namespace DressShop.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 10000)]
         public double Price100 { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 
 }
