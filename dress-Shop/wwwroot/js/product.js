ac9233e006b2a7792d1838314a2c1204d2ae45eb
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-26 13:37:05 +0100

    .

--- dress-Shop/wwwroot/js/product.js
+++ dress-Shop/wwwroot/js/product.js
@@ -15,7 +15,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return '<div class="w-75 btn-group" role="group"><a href="" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a> <a href = "" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a></div> '
                 },
-                "width": "15%"
+                "width": "20%"
             }
 
         ]
