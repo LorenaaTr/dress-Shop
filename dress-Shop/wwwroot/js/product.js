f13fde924ceb40e9043c9649ef58d524aa41ea3e
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-29 20:23:30 +0100

    updates

--- dress-Shop/wwwroot/js/product.js
+++ dress-Shop/wwwroot/js/product.js
@@ -13,7 +13,10 @@ function loadDataTable() {
             {
                 data: 'id',
                 "render": function (data) {
-                    return '<div class="w-75 btn-group" role="group"><a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a> <a href = "" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a></div> '
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
+                    <a href="/admin/product/delete/${data}" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
+                       </div>`
                 },
                 "width": "20%"
             }
