963f397c8d3736d6693d58fcbe691207c426b733
LorenaaTr <lorenatroshupa@gmail.com>
2023-12-31 10:35:55 +0100

    Home Page

--- dress-Shop/Areas/Admin/Controllers/ProductController.cs
+++ dress-Shop/Areas/Admin/Controllers/ProductController.cs
@@ -110,7 +110,8 @@ namespace dress_Shop.Areas.Admin.Controllers
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new { data = objProductList });
         }
-    
+
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
             var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id==id);
