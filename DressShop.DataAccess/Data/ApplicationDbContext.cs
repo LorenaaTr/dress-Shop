d2b6ebd1d468d42051d8eee2b96822367c30bdac
LorenaaTr <lorenatroshupa@gmail.com>
2024-01-01 11:43:41 +0100

    Implemented Identity

--- DressShop.DataAccess/Data/ApplicationDbContext.cs
+++ DressShop.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using DressShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DressShop.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -13,6 +14,9 @@ namespace DressShop.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Elegant", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Simple", DisplayOrder = 2 },
